4da317fdeb3afc8446e9c713ba397ad0728c0184
maexmaex <max@walkbase.com>
2017-04-07 18:19:53 +0300

    Added more use cases

--- gantry.py
+++ gantry.py
@@ -3,22 +3,26 @@
 # Software Safety
 # Assignment 1 
 # Gantry Controller
-# Benjamin Bergens, Niklas Simons, Marcus Norrgård
+# Benjamin Bergens, Niklas Simons, Marcus Norrgard
 
 import sys
 import argparse
 import random
 import time
 
+# Gantry controller
 def main(x_next):
-	# Magnet starts released
+	# Magnet starts as released
 	gantryLoaded = False
 	print "#### GANTRY OPERATION TERMINAL 1.0 ####"
 	x_next = int(x_next)
 	simulateMove(2)
 	print "Object at position x: " + str(x_next)
-	x_now = s_x()
+	x_now = startMotor(gantryLoaded)
 	print "Gantry at postition x: " + str(x_now)
+	#checking the x_values
+	inputSanityCheck(x_next)
+	sensorSanityCheck(x_now)
 	simulateMove(2)
 	print "Scheaduling pickup!" 
 	simulateMove(2)
@@ -26,15 +30,28 @@ def main(x_next):
 	simulateMove(2)
 	print "Taking object to loading bay"
 	returnToLoadingBay(x_now, gantryLoaded)
+
+def inputSanityCheck(x_next):
+	if x_next > 10 or x_next < 0:
+		panicHaltGracefully("Opertor placed object out of bounds, halting!!!")
+def sensorSanityCheck(x_now):
+	if x_now > 10:
+		panicHaltGracefully("S_X sensor saturated. Halting!!!")
+
+def startMotor(gantryLoaded):
+	start = random.randint(0,3)
+	if start==3:
+		panicHaltGracefully("Engine failed to start!!!")
+	print "starting engines..."
+	return s_x("start", "")
+
 # Method triggers the gantry to move back to the loading bay
 def returnToLoadingBay(x_now, gantryLoaded):
 	bay = s_left(x_now)
 	while bay!=True:
-		motorL()
-		x_now = x_now-1
+		x_now = motorL(x_now)
 		# Check with s_left sensor where we are after each move
 		bay = s_left(x_now)
-		print "Gantry at x: " + str(x_now)
 	toggleMagnet(gantryLoaded)
 # Movement controller, handels the motors and the magnet
 def move(x_next, x_now, gantryLoaded):
@@ -43,17 +60,13 @@ def move(x_next, x_now, gantryLoaded):
 		if x_next > x_now:					
 			end = s_right(x_now)
 			if end:
-				panicHalt()
-			motorR()
-			x_now=x_now+1
-			print "Gantry at x: " + str(x_now)
+				panicHalt(gantryLoaded, "Input wants me to go on, but I have reached the end of the boom, I will halt.")
+			x_now = motorR(x_now)			
 		if x_next < x_now:
 			end = s_left(x_now)	
 			if end:
-				panicHalt()
-			motorL()
-			x_now = x_now-1	
-			print "Gantry at x: " + str(x_now)
+				panicHalt(gantryLoaded, "Input wants me to go on, but I have reached the end of the boom, I will halt.")
+			x_now = motorL(x_now)
 		if x_next == x_now:
 			print "Gantry on top of object"
 			gantryLoaded = toggleMagnet(gantryLoaded)
@@ -77,17 +90,31 @@ def toggleMagnet(gantryLoaded):
 def simulateMove(x):
 	time.sleep(x)
 # Motor to move gantry to the left
-def motorL():
+# have s_x read the new x-value
+def motorL(x_now):	
 	time.sleep(2)
 	print "<"
+	return s_x(x_now, "L")
 # Motor to move gantry to the right
-def motorR():
+# have s_x read the new x-value
+def motorR(x_now):
 	time.sleep(2)
 	print ">"
+	return s_x(x_now, "R")
 # The gantry sensor that returns the x value for the gantrys position
-def s_x():
-	x_now = random.randint(0,11)
-	return x_now
+# s_x can fail and produce a number greater than 10, gantry bounds are 0 - 10
+def s_x(x_now, direction):
+	if x_now=="start" and direction=="":
+		x_now_init = random.randint(0,15)	
+		return x_now_init
+	if direction=="L":
+		x_now=x_now-1
+		print "Gantry at x: " + str(x_now)
+		return x_now
+	if direction=="R":
+		x_now=x_now+1
+		print "Gantry at x: " + str(x_now)
+		return x_now
 # The right sensor
 def s_right(x_now):
 	if x_now == 10:
@@ -98,11 +125,22 @@ def s_left(x_now):
 	if x_now == 0:
 		return True
 	else: False
-def panicHalt():
+def panicHalt(gantryLoaded, msg):
 	simulateMove(2)
-	print "Input wants me to go on, but I have reached the end of the boom, I will halt."
+	print msg
+	toggleMagnet(gantryLoaded)
 	sys.exit()
-	
+# halt with the 
+def panicHaltGracefully(msg):
+	print msg
+	sys.exit()
+# Method not in use. Should simulate a randomly occuring engine failure. Could be done by utilizing threads!
+def enigneFailiure():
+	for i in range(random.randint(10,15)):
+		time.sleep(1)
+	print "Engine malfunction!!!"
+	sys.exit()
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Example usage: python gantry.py x_next")
 	parser.add_argument('x_next', help="Set between 0 and 10")
